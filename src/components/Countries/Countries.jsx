e470dc82aafc3231eb2f8ff70d178edc3a029ca3
Imtiaz <im3azchowdhury@gmail.com>
2023-09-11 09:38:48 +0600

    first commit

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,47 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Country from "../Country/Country";
+import "./Countries.css";
+
+const Countries = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+
+  const [visitedCountries, setVisitedCountries] = useState([]);
+
+  const handleVisitedCountry = (country) => {
+    // console.log("Add this to your visited country");
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+  };
+
+  return (
+    <div>
+      <h3>Countries: {countries.length} </h3>
+      <div>
+        <h4>Visited Countries: {visitedCountries.length}</h4>
+        <ul>
+          {
+            visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+          }
+        </ul>
+      </div>
+      <div className="country-container">
+        {countries.map((country) => (
+          <Country
+            key={country.cca2}
+            handleVisitedCountry={handleVisitedCountry}
+            country={country}
+          ></Country>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Countries;
